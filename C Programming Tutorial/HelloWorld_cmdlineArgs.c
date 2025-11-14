7eae9bb1c3fcfb34dc61041ba001f3dd4e9c53b5
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-14 20:51:50 +0000

    Create HelloWorld_cmdlineArgs.c example
    Add a simple C program demonstrating command-line arguments.

---
+++ C Programming Tutorial/HelloWorld_cmdlineArgs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  // Overview of how we can specify command-line arguments via int main()
+  // cmdline inputs assigned via argc/argv 
+  printf("Hello World!\n");
+  return(0);
+}
