51e141ea8b2f377ca0c357f493e30c16c14b1376
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-14 20:57:55 +0000

    Fix return statement formatting in HelloWorld_cmdlineArgs

--- C Programming Tutorial/HelloWorld_cmdlineArgs.c
+++ C Programming Tutorial/HelloWorld_cmdlineArgs.c
@@ -4,5 +4,5 @@ int main(int argc, char* argv[]) {
   // Overview of how we can specify command-line arguments via int main()
   // cmdline inputs assigned via argc/argv 
   printf("Hello World!\n");
-  return(0);
+  return 0;
 }
