14807e2564cb52acae4e92efcc513a259ffdc723
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-18 19:23:03 +0000

    Update reset.php

--- PLAYGROUND/OS Command Injection Vuln Environment/reset.php
+++ PLAYGROUND/OS Command Injection Vuln Environment/reset.php
@@ -1,10 +1,21 @@
 <?php
  // resetting all session values 
  // redirecting to "UserCP.php" after session values are reset
+function reset() {
     session_start();
     print_r($_SESSION);
     session_unset();
     session_destroy();
     header('Location: /UserCP.php');
     exit;
+}
+
+function logout() {
+    session_start();
+    session_unset();
+    unset($_SESSION);
+    session_destroy();
+    header('Location: /');
+}
+
 ?>
