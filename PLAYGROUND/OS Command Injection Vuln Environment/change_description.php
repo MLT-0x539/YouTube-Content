d61a55d91dd0794df582192d7ca2fc9293d7e1d3
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-18 17:10:36 +0000

    Add change_description.php for user profile updates

---
+++ PLAYGROUND/OS Command Injection Vuln Environment/change_description.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+ <?php
+  session_start();
+  error_reporting(0);
+ ?>
+
+ <head>
+   <meta charset="UTF-8">
+   <title> Change Profile Description </title>
+ </head>
+
+  <body>
+    <a href="admin_login.php"> LOGIN TO ADMINCP </a>
+   <br />
+    <a href="user_logout.php"> LOG OUT OF ACCOUNT </a>
+   <br />
+    <a href="UserCP.php"> RETURN TO USER CONTROL PANEL </a>
+   <br />
+    <a href="user_profile.php"> RETURN TO USER PROFILE </a>
+   <br />  
+
+   <center>
+    <h1><b><u>Page to change your user profile dsecription:</u></b></h1>
+   </center>
+  <hr>
+  <br />
+  <br />
+
+<!-- BONUS: Blind XSS can be demonstrated via this input -->
+
+  <form action="user_profile.php">
+    <p><label for="profile-desec">Change user profile description here:</label></p>
+    <textarea id=""profile-desec" name=""profile-desec" rows="4" cols="50">UPDATE THE CONTENTS OF THIS FORM IF YOU WANT TO CHANGE YOUR PROFILE DESCRIPTION</textarea>
+   <br>
+    <input type="submit" value="Submit">
+  </form>
+  <br />
+
+ </body>
+</html>
