e95fd03eac584b23c0b616cf4521208ea24bdcb2
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-18 17:37:41 +0000

    Add user profile page with navigation links

---
+++ PLAYGROUND/OS Command Injection Vuln Environment/profile.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+error_reporting(0);
+require_once(
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title> User Profile </title>
+</head>
+  <body>
+      <body>
+    <a href="admin_login.php"> LOGIN TO ADMINCP </a>
+   <br />
+    <a href="user_logout.php"> LOG OUT OF ACCOUNT </a>
+   <br />
+    <a href="UserCP.php"> RETURN TO USER CONTROL PANEL </a>
+   <br />
+    <a href="user_profile.php"> RETURN TO USER PROFILE </a>
+   <br />  
+    <a href="reset.php"> RESET TO ALL DEFAULT VALUES </a>
+   <br />  
+
+  <center>
+    <h1><b><u> User Profile: </u></b></h1>
+  </center>
+  <hr>
+  <textarea id="story" name="story" rows="10" cols="50">
+   <?pnp echo new_desc; ?>
+  </textarea>
+  </body>
+  
+</html>
