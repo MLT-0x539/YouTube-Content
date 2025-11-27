b00cc3d5d5b8277b63ea1ae90d45c5c0e9db3669
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-27 21:47:54 +0000

    Create for_loop.c

---
+++ C Programming Tutorial/for_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+ int a,b;
+
+for (a = 0; a <= 10; a++) {
+   printf("Value is: %d\n", a); 
+     // Counts up from 0 to 10
+ }
+
+printf("\n\n");
+
+for (b = 10; b >= 0; b--) {
+   printf("Value is: %d\n", b);
+     // Counts down from 10 to 0
+ }
+
+return(0);
+
+}
